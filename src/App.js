38a3474b014b02520796689004c65c792f967c39
Kevin Knights <kevinknights29@hotmail.com>
2023-11-20 21:23:16 -0500

    WIP: adding multistep form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import {AppProvider} from './context/state';
+import {MultiStepForm} from './components/MultiStepForm/MultiStepForm';
 import './App.css';
-import DataForm from './components/DataForm/DataForm';
 
+/**
+ * Renders the main application component.
+ * @return {JSX.Element} The rendered application component.
+ */
 function App() {
   return (
-    <div className="App">
-      <DataForm />
-    </div>
+    <AppProvider>
+      <MultiStepForm/>
+    </AppProvider>
   );
 }
 
